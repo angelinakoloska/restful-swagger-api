7099b24a1050d26e082e9397cf61a5c7d339000f
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-11 13:50:41 +0100

    Add the routes for user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var UserService = require('../services/UserService');
+var db = require('../models');
+var userService = new UserService(db);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  let users = await userService.getAll();
+  res.send(users);
+});
+
+router.post('/', jsonParser, async function(req, res, next) {
+  let username = req.body.Username;
+  let password = req.body.Password;
+  let score = req.body.Score;
+  await userService.create(username, password, score)
+  res.end();
+});
+
+router.delete('/:id', jsonParser, async function(req, res, next) {
+  await userService.delete(req.params.id);
+  res.end();
 });
 
 module.exports = router;
