605e90e8161b6514cf1a06fd7348f92b69539baf
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-11 13:37:12 +0100

    Add user service

---
+++ services/UserService.js
@@ -0,0 +1,29 @@
+class UserService {
+    constructor(db) {
+        this.client = db.sequelize;
+        this.User = db.User;
+    }
+
+    async create(username, password, score) {
+        return this.User.create({
+            Username: username,
+            Password: password,
+            Score: score
+        });
+    }
+
+
+    async getAll() {
+        return await this.User.findAll({
+            where: {}
+        })
+    }
+
+    async delete(id) {
+        return this.User.destroy({
+            where: {id: id}
+        })
+    }
+}
+
+module.exports = UserService;
