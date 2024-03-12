f9d021a2a2a6a968b7e06a2c2e1ccd945ed2a0a7
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-12 16:08:56 +0100

    Modify the user route

--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-var UserService = require('../services/UserService');
-var db = require('../models');
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var db = require("../models");
+var UserService = require("../services/UserService")
 var userService = new UserService(db);
-
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  //swagger.tags = ['Users]
-  //swagger.description = "Gets the list of all users"
-  let users = await userService.getAll();
-  res.send(users);
+    // #swagger.tags = ['Users']
+    // #swagger.description = "Gets the list of all users."
+    var users = await userService.getAll()
+    res.send(users);
 });
-
 router.post('/', jsonParser, async function(req, res, next) {
-  //swagger.tags = ['Users]
-  //swagger.description = "Adds the user based on parameters provided in the request's body"
-  /* #swagger.parameters['body'] =  {
-    "name": "body",
-    "in": "body",
-      "schema": {
-        $ref: "#/definitions/User"
-      }
-    }
-  */
-  let username = req.body.Username;
-  let password = req.body.Password;
-  let score = req.body.Score;
-  await userService.create(username, password, score)
-  res.end();
+    // #swagger.tags = ['Users']
+    // #swagger.description = "Adds the user based on parameters provided in the request's body"
+    /* #swagger.parameters['body'] = {
+          "name": "body",
+          "in": "body",
+          "schema": {
+              "$ref": "#/definitions/User"
+          }
+      } */
+    const {username, password, score} = req.body
+    await userService.create(username, password, score);
+    res.end();
 });
-
-router.delete('/:id', jsonParser, async function(req, res, next) {
-    //swagger.tags = ['Users]
-    //swagger.description = "Deletes a user of ID provided in the request's body"
-  await userService.delete(req.params.id);
-  res.end();
+router.delete('/', jsonParser, async function(req, res, next) {
+    // #swagger.tags = ['Users']
+    // #swagger.description = "Deletes a user of ID provided in the request's body."
+    await userService.deleteUser(req.body.id);
+    res.end();
 });
-
 module.exports = router;
