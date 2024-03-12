bf1214ec82ee123fab06110a8cacdef9c6c6a489
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-12 16:14:59 +0100

    Make some changes

--- services/UserService.js
+++ services/UserService.js
@@ -1,22 +1,22 @@
 class UserService {
     constructor(db) {
         this.client = db.sequelize;
-        this.user = db.User;
+        this.User = db.User;
     }
-    async create(username, password, score) {
-        return this.user.create({
-            Username: username,
-            Password: password,
-            Score: score
+    async create(Username, Password, Score) {
+        return this.User.create({
+            Username: Username,
+            Password: Password,
+            Score: Score
         });
     }
     async getAll() {
-        return this.user.findAll({
+        return this.User.findAll({
             where: {}
         });
     }
     async deleteUser(userId) {
-        return this.user.destroy({
+        return this.User.destroy({
             where: { id: userId }
         });
     }
