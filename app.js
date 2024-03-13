84c1d98df1387ae87c43b469232f95e04c5e270c
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-13 17:02:11 +0100

    Add third-party login

--- app.js
+++ app.js
@@ -1,50 +1,58 @@
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger-output.json')
-const bodyParser = require('body-parser')
 require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var db = require("./models");
-db.sequelize.sync({ force: false })
+db.sequelize.sync({ force: false, alter: true })
+
+const { auth } = require('express-openid-connect');
+const config = {
+authRequired: false,
+auth0Logout: true
+};
 
 var app = express();
+config.baseURL = `http://localhost:${process.env.PORT}`;
+app.use(auth(config));
+app.use(function (req, res, next) {
+res.locals.user = req.oidc.user;
+next();
+});
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+app.use('/doc', swaggerUi.serve);
+app.get('/doc', swaggerUi.setup(swaggerDocument));
 
-// view engine setup
+//... view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(bodyParser.json())
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// set locals, only providing error in development
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err :
+{};
+// render the error page
+res.status(err.status || 500);
+res.render('error');
 });
 
 module.exports = app;
